5ec7d0020619d4e6cd75a58141c4c235bae73571
RafaelCoelho1803 <rcoelho_de_oliveira@hotmail.com>
2024-04-09 18:59:04 -0300

    Updates

--- resultados.php
+++ resultados.php
@@ -15,19 +15,21 @@ $ano = isset($_GET['ano']) ? $_GET['ano'] : '22'; // Valor padrão: 22
 $produto = isset($_GET['produto']) ? $_GET['produto'] : 'S'; // Valor padrão: Soja
 
 // Construção da consulta SQL com base nos filtros
-$query_talhao = "SELECT t.id_talhao, t.Area, SUM(p.peso_bruto - f.peso) AS peso_liquido, 
-                 ROUND(SUM((p.peso_bruto - f.peso) / 60)) AS sacos,
-                 ROUND(SUM((p.peso_bruto - f.peso) / (60 * t.Area))) AS media_sacos_por_area
-                 FROM talhoes t 
-                 LEFT JOIN pesagem p ON t.id_talhao = p.talhao_id 
-                 LEFT JOIN frete f ON p.frete_id = f.id_frete 
-                 WHERE t.user_id = $user 
-                 AND p.ano = $ano
-                 AND p.produto = '$produto'
-                 GROUP BY t.id_talhao";
+$query_talhao = "SELECT t.id_talhao, t.Area, 
+                SUM(p.peso_liquido) AS peso_liquido, 
+                ROUND(SUM((peso_liquido) / 60)) AS sacos,
+                ROUND(SUM((peso_liquido) / (60 * t.Area))) AS media_sacos_por_area,
+                SUM(p.desconto) AS total_desconto,
+                COUNT(p.id_pesagem) AS total_pesagens
+                FROM talhoes t 
+                LEFT JOIN pesagem p ON t.id_talhao = p.talhao_id 
+                LEFT JOIN frete f ON p.frete_id = f.id_frete 
+                WHERE t.user_id = $user 
+                AND p.ano = $ano
+                AND p.produto = '$produto'
+                GROUP BY t.id_talhao";
                  
 $result_talhao = mysqli_query($conn, $query_talhao);
-
 
 // Verifica se houve erro na consulta SQL
 if (!$result_talhao) {
@@ -45,7 +47,14 @@ mysqli_close($conn);
     <title>Caminhoes</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-
+    <style>
+        .Top-side {
+            margin-bottom: 20px;
+        }
+        .Resultados {
+            margin-top: 20px;
+        }
+    </style>
 </head>
 
 <body>
@@ -72,62 +81,66 @@ mysqli_close($conn);
             </nav>
         </div>
     </div>
-    <div class="Back-side">
-        <form class="row gx-3 gy-2 align-items-center m-4 " method="get">
-            
-            <div class="col-">
+    <div class="container-fluid Back-side">
+        <form class="row gx-3 gy-2 align-items-center m-4" method="get">
+            <div class="col-sm">
                 <label class="visually-hidden" for="specificSizeSelect">Ano</label>
                 <select class="form-select" id="specificSizeSelect" name="ano">
-                <option <?php if ($ano == '22') echo 'selected' ?>>22</option>
-                <option <?php if ($ano == '23') echo 'selected' ?>>23</option>
-                <option <?php if ($ano == '24') echo 'selected' ?>>24</option>
-                <option <?php if ($ano == '25') echo 'selected' ?>>25</option>
+                    <option <?php if ($ano == '24') echo 'selected' ?>>24</option>
+                    <option <?php if ($ano == '25') echo 'selected' ?>>25</option>
+                    <option <?php if ($ano == '26') echo 'selected' ?>>26</option>
+                    <option <?php if ($ano == '27') echo 'selected' ?>>27</option>
                 </select>
             </div>
-            <div class="col-sm-2">
+            <div class="col-sm">
                 <label class="visually-hidden" for="specificSizeSelect">Produto</label>
                 <select class="form-select" id="specificSizeSelect" name="produto">
-                <option value="S" <?php if ($produto == 'S') echo 'selected' ?>>Soja</option>
-                <option value="M" <?php if ($produto == 'M') echo 'selected' ?>>Milho</option>
+                    <option value="S" <?php if ($produto == 'S') echo 'selected' ?>>Soja</option>
+                    <option value="M" <?php if ($produto == 'M') echo 'selected' ?>>Milho</option>
+                </select>
+            </div>    
+            <div class="col-sm">    
+                <label class="visually-hidden" for="specificSizeSelect">Resultados</label>
+                <select class="form-select" id="specificSizeSelect" name="produto">
+                    <option value="T">Talhao</option>
+                    <option value="G">Gerais</option>
                 </select>
             </div>
             <div class="col-auto">
                 <button type="submit" class="btn btn-primary">Filtrar</button>
             </div>
-            
         </form>
-        <div class="Resultados">
+        <div class="Resultados m-4">
             <div class="row">
-                <div class="col-5">
-                    <table class="table">
+                <div class="col">
+                    <table class="table table-striped">
                         <thead>
                             <tr>
-                            <th scope="col">Talhao</th>
-                            <th scope="col">Áreas</th>
-                            <th scope="col">Peso Líquido</th>
-                            <th scope="col">Sacos</th>
-                            <th scope="col">Média de Sacos por Área</th>
+                                <th scope="col">Talhao</th>
+                                <th scope="col">Áreas(He)</th>
+                                <th scope="col">Peso Líquido(Kg)</th>
+                                <th scope="col">Sacos</th>
+                                <th scope="col">Média de Sacos</th>
+                                <th scope="col">Desconto da Área</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php                               
-                                // Loop through the results and display each row in the table
-                                while ($row = mysqli_fetch_assoc($result_talhao)) {
-                                    echo "<tr>";
-                                    echo "<td>" . $row['id_talhao'] . "</td>";
-                                    echo "<td>" . $row['Area'] . "</td>";
-                                    echo "<td>" . $row['peso_liquido'] . "</td>";
-                                    echo "<td>" . $row['sacos'] . "</td>";
-                                    echo "<td>" . $row['media_sacos_por_area'] . "</td>";
-                                    echo "</tr>";
-                                }
+                            // Loop through the results and display each row in the table
+                            while ($row = mysqli_fetch_assoc($result_talhao)) {
+                                echo "<tr>";
+                                echo "<td>" . $row['id_talhao'] . "</td>";
+                                echo "<td>" . $row['Area'] . "</td>";
+                                echo "<td>" . $row['peso_liquido'] . "</td>";
+                                echo "<td>" . $row['sacos'] . "</td>";
+                                echo "<td>" . $row['media_sacos_por_area'] . "</td>";
+                                $media_desconto = $row['total_desconto'] / $row['total_pesagens'];
+                                echo "<td>" . $media_desconto . "</td>";
+                                echo "</tr>";
+                            }
                             ?>                               
                         </tbody>
                     </table>
-                </div>
-            
-                <div class="col-5">
-                    
                 </div>
             </div>
         </div>
